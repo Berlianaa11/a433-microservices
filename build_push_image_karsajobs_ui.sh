944de4c42d71a5040d347fb52a325cc46d47d6e2
Berliana <133011155+Berlianaa11@users.noreply.github.com>
2023-11-29 08:17:34 +0700

    Add files via upload

---
+++ build_push_image_karsajobs_ui.sh
@@ -0,0 +1,8 @@
+#Perintah untuk membuat docker image dengan nama karsajobs-ui dan dengan tag latest
+docker build -t docker.pkg.github.com/berlianaa11/karsajobs-app/karsajobs-ui:latest .
+
+#Perintah login ke Github Packages
+docker login docker.pkg.github.com -u berlianaa11 -p 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
+
+#Perintah untuk mengupload docker image ke Github Packages
+docker push docker.pkg.github.com/berlianaa11/karsajobs-app/karsajobs-ui:latest
