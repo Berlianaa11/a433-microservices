7a3183fa4287a6d67e14534a7447db6c561ff0bd
Berliana <133011155+Berlianaa11@users.noreply.github.com>
2023-11-30 23:53:39 +0700

    Add files via upload

---
+++ build_push_image_shipping_service.sh
@@ -0,0 +1,8 @@
+#Perintah untuk membuat docker image dengan nama shipping-service dan dengan tag latest
+docker build -t docker.pkg.github.com/berlianaa11/e-commerce-app/shipping-service:latest .
+
+#Perintah login ke Github Packages
+docker login docker.pkg.github.com -u berlianaa11 -p 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
+
+#Perintah untuk mengupload docker image ke Github Packages
+docker push docker.pkg.github.com/berlianaa11/e-commerce-app/shipping-service:latest
